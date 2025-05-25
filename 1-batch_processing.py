591c200662f105e0348b4864ff1a24c59e11be4b
Adnan Hamisi <ahamisi777@gmail.com>
2025-05-25 20:46:01 +0300

    Batch processing Large Data

--- 1-batch_processing.py
+++ 1-batch_processing.py
@@ -1,6 +1,7 @@
-import seed
+import seed  # Assuming seed.py has your DB connection functions
 
 def stream_users_in_batches(batch_size):
+    """Generator that fetches user_data rows in batches from the database."""
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     offset = 0
@@ -16,7 +17,9 @@ def stream_users_in_batches(batch_size):
     cursor.close()
     connection.close()
 
+
 def batch_processing(batch_size):
+    """Process batches to yield only users older than 25."""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
